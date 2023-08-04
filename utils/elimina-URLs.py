59e08d711a2c0b4cfd653abb12a9f409581a862e
JJ Merelo <jjmerelo@gmail.com>
2023-08-04 11:03:58 +0200

    Elimina referencias closes #3

--- utils/elimina-URLs.py
+++ utils/elimina-URLs.py
@@ -11,12 +11,14 @@ else:
     dir = "pois/"
 
 ficheros = glob.glob(dir+"/*.md")
-ficheros.append( "intro.md" )
+ficheros.append("intro.md")
 
 for f in ficheros:
-    file_content = open(f,"r").read()
-    file_content = re.sub(r'\[([^\]]+)\]\(http[^\(]+\)',r'\1', file_content)
+    file_content = open(f, "r").read()
+    file_content = re.sub(r'\[([^\]]+)\]\(http[^\(]+\)', r'\1', file_content)
+    split_refs = re.split(r'## Referencias\s+', file_content)
+    file_content = split_refs[0]
 
-    f = f.replace('.md','-nolinks.md')
+    f = f.replace('.md', '-nolinks.md')
     with open(f, "w") as links_file:
-        links_file.write( file_content )
+        links_file.write(file_content)
